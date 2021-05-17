a7eb9899a069a144963003dcad52942dcfd2ff8a
spencerstith <spencerstith@yahoo.com>
2021-05-17 10:03:47 -0500

    Just goofing around

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send("you are at home!");
+})
+
 app.get('/about', (req, res) => {
     res.send("hey");
 });
