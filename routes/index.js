c3be394afe47a01adf54a5a15aeb5c9002843380
spencerstith <spencerstith@yahoo.com>
2021-05-18 09:19:15 -0500

    Working on Navbar

--- routes/index.js
+++ routes/index.js
@@ -12,16 +12,16 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 })
 
-router.get('/construction', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
-
 router.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/projects.html'));
+    res.sendFile(path.join(__dirname, '../pages/projects.html'));
 });
 
 router.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/about.html'));
+    res.sendFile(path.join(__dirname, '../pages/about.html'));
+});
+
+router.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pages/contact.html'));
 });
 
 module.exports = router;
