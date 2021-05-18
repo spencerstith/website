8794454fdb35261eafeb6e621f0fbd4b4bed4253
spencerstith <spencerstith@yahoo.com>
2021-05-18 10:46:45 -0500

    Heroku Push

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,6 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
-/* GET home page. */
-/*
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-*/
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 })
