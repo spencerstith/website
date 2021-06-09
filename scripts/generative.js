16580d0b23030c256179b5d6149b96b5499eb8c4
spencerstith <spencerstith@yahoo.com>
2021-06-08 20:56:10 -0500

    Much better formatting

--- scripts/generative.js
+++ scripts/generative.js
@@ -7,16 +7,17 @@ function loadGenProjects() {
             const columns = $('<div class="columns is-desktop is-vcentered">');
 
             var image = $(`<img src="generative/${proj.image}">`);
-            var figure = $('<figure class="image is-128x128">').html(image);
-            var imageCol = $('<div class="column">').html(figure);
+            var figure = $('<figure class="image">').append(image);
+            var imageCol = $('<div class="column">').append(figure);
 
             var title = $(`<h1 class="title">${proj.title}</h1>`);
-            var titleCol = $('<div class="column">').html(title);
+            var desc = $(`<p>${proj.description}</p>`);
 
-            var desc = $(`<p class="${proj.description}">Gens</p>`);
-            var descCol = $('<div class="column">').html(desc);
+            var descCol = $('<div class="column">');
+            descCol.append(title);
+            descCol.append(desc);
 
-            columns.append(imageCol).append(titleCol).append(descCol);
+            columns.append(imageCol).append(descCol);
 
             var box = $('<div class="box">').html(columns);
             $("#" + col).append(box);
